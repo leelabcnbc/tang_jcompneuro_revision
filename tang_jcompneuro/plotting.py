5aa29131908f970a7ee7048c42b2ff13fd12b4fb
Yimeng Zhang <zym1010@gmail.com>
2018-03-30 10:29:17 -0400

    more plots done

--- tang_jcompneuro/plotting.py
+++ tang_jcompneuro/plotting.py
@@ -162,7 +162,9 @@ def show_perf_vs_size(x_list: List[np.ndarray],
                       style_list=None,
                       linewidth=1,
                       show_legend=True,
-                      vline=None
+                      vline=None,
+                      hline=None,
+                      # letter=None,
                       ):
     """x being model size, number of parameter, dataset size, etc.
     y being performance.
@@ -171,6 +173,10 @@ def show_perf_vs_size(x_list: List[np.ndarray],
     if style_list is None:
         # should give a default set
         raise NotImplementedError
+
+    # if letter is not None:
+    #     ax.text(0, 1, letter, horizontalalignment='left', verticalalignment='top',
+    #             transform=ax.get_figure().transFigure, fontweight='bold')
 
     assert len(x_list) == len(y_list) == len(label_list)
     for idx, (x_this, y_this, label_this) in enumerate(zip(x_list, y_list, label_list)):
@@ -181,7 +187,11 @@ def show_perf_vs_size(x_list: List[np.ndarray],
 
     if vline is not None:
         # color maybe adjusted later
-        ax.axvline(vline, color='r', linewidth=linewidth)
+        ax.axvline(vline, color='black', linewidth=linewidth, linestyle='--')
+
+    if hline is not None:
+        # color maybe adjusted later
+        ax.axhline(hline, color='black', linewidth=linewidth, linestyle='--')
 
     # ax.set_xlim(0, 1)
     ax.set_xlim(*xlim)
@@ -205,7 +215,8 @@ def show_perf_vs_size(x_list: List[np.ndarray],
 def show_one_decomposed_bar(stat_chunks_array, stat_name_array, *,
                             ax: Axes = None, xlabel=None,
                             title=None,
-                            color_bias: int = None, set_ylabel=False):
+                            color_bias: int = None, set_ylabel=False,
+                            ylabel_styles=None, letter_map=None):
     # https://github.com/leelabcnbc/tang_jcompneuro/blob/master/thesis_plots/v1_fitting/comparison_among_all_non_vgg_models_decomposed_by_fine_subsets.ipynb
     color_list = plt.get_cmap('Set2').colors
 
@@ -216,6 +227,9 @@ def show_one_decomposed_bar(stat_chunks_array, stat_name_array, *,
 
     if ax is None:
         ax = plt.gca()
+    if letter_map is not None:
+        ax.text(-0.02, 1, chr(letter_map + ord('a')), horizontalalignment='right', verticalalignment='top',
+                transform=ax.transAxes, fontweight='bold')
 
     n_model = len(stat_name_array)
 
@@ -231,7 +245,7 @@ def show_one_decomposed_bar(stat_chunks_array, stat_name_array, *,
     ax.set_ylim(0, n_model + 2)
 
     if set_ylabel:
-        ax.set_yticks(np.arange(n_model)+1)
+        ax.set_yticks(np.arange(n_model) + 1)
         ax.set_yticklabels(stat_name_array, fontdict={'fontsize': 'medium'})
 
     if xlabel is not None:
@@ -243,6 +257,101 @@ def show_one_decomposed_bar(stat_chunks_array, stat_name_array, *,
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
 
+    if ylabel_styles is not None:
+        assert len(ylabel_styles) == len(stat_name_array)
+        # https://stackoverflow.com/questions/24617429/matplotlib-different-colors-for-each-axis-label
+        for ytick, style in zip(ax.get_yticklabels(), ylabel_styles):
+            if style is not None:
+                if style in {'bold', 'semibold'}:
+                    ytick.set_weight(style)
+                elif style in {'italic'}:
+                    ytick.set_style(style)
+                else:
+                    raise NotImplementedError
 
-def show_one_decomposed_scatter():
-    pass
+
+def show_one_decomposed_scatter(raw_chunks_x, raw_chunks_y, *,
+                                ax: Axes=None, xlabel=None, ylabel=None,
+                                title=None,
+                                color_bias: int = None, letter_map=None):
+    if letter_map is not None:
+        ax.text(0, 1, chr(letter_map + ord('a')),
+                horizontalalignment='left',
+                verticalalignment='top', fontweight='bold',
+                transform=ax.transAxes)
+
+    # this is for spotlight
+    assert len(raw_chunks_x) == len(raw_chunks_y)
+    color_list = plt.get_cmap('Set2').colors
+    chunk_x_all, chunk_y_all = [], []
+    for idx, (raw_chunk_this_x, raw_chunk_this_y) in enumerate(zip(raw_chunks_x, raw_chunks_y)):
+        assert raw_chunk_this_x.shape == raw_chunk_this_y.shape == (raw_chunk_this_y.size,)
+
+        # only sample up to 50 points.
+        if raw_chunk_this_x.size > 50:
+            rng_state_this = np.random.RandomState(seed=0)
+            index_rand = rng_state_this.choice(raw_chunk_this_x.size, 50, replace=False)
+        else:
+            index_rand = slice(None)
+
+        ax.scatter(raw_chunk_this_x[index_rand], raw_chunk_this_y[index_rand],
+                   color=color_list[color_bias + idx], alpha=0.5,
+                   s=12)
+        # show linear regression line.
+        fit_this = np.polyfit(raw_chunk_this_x, raw_chunk_this_y, deg=1)
+
+        #         start_end_vector_this = np.array([raw_chunk_this_x.min(),raw_chunk_this_x.max()])
+        start_end_vector_this = np.array([0.5, 1])
+        ax.plot(start_end_vector_this, (fit_this[0] * (start_end_vector_this - 0.5) * 2 + fit_this[1]) / 2,
+                color=color_list[color_bias + idx], linewidth=1)
+
+        #         ax.text(0.95,0.6-idx*0.1, '{:.3f}'.format(fit_this[0]),
+        #                 horizontalalignment='right',
+        #                 verticalalignment='top', fontsize='medium',
+        #                 color=color_list[color_bias+idx])
+
+        chunk_x_all.append(raw_chunk_this_x.copy())
+        chunk_y_all.append(raw_chunk_this_y.copy())
+
+        # add text.
+
+    chunk_x_all = np.concatenate(chunk_x_all)
+    chunk_y_all = np.concatenate(chunk_y_all)
+    fit_this = np.polyfit(chunk_x_all, chunk_y_all, deg=1)
+    #     start_end_vector_this = np.array([chunk_x_all.min(),chunk_x_all.max()])
+    start_end_vector_this = np.array([0.5, 1])
+    # linear transform things values in [0,1] x [0,1] to [0.5,1] x [0,0.5]
+    ax.plot(start_end_vector_this, (fit_this[0] * (start_end_vector_this - 0.5) * 2 + fit_this[1]) / 2, color='black',
+            linewidth=1, linestyle='--')
+
+    ax.plot([0, 1], [0, 1], linestyle='--')
+    ax.set_xlim(-0.15, 1.1)
+    ax.set_ylim(-0.15, 1.1)
+    ax.axis('off')
+
+    if title is not None:
+        ax.text(0.5, 0.975, title,
+                horizontalalignment='center',
+                verticalalignment='top', fontsize='medium',
+                transform=ax.transAxes)
+    if xlabel is not None:
+        ax.text(0.5, 0, xlabel,
+                horizontalalignment='center',
+                verticalalignment='top', fontsize='medium',
+                )
+    if ylabel is not None:
+        ax.text(0, 0.5, ylabel, rotation='vertical',
+                horizontalalignment='right',
+                verticalalignment='center', fontsize='medium',
+                )
+
+    # add pearson stuff
+    ax.text(0, 1, '{:.4f}'.format(chunk_y_all.mean()),
+            horizontalalignment='left',
+            verticalalignment='top', fontsize='medium')
+    ax.text(1, 0, '{:.4f}'.format(chunk_x_all.mean()),
+            horizontalalignment='right',
+            verticalalignment='bottom', fontsize='medium')
+    corr_this = pearsonr(chunk_x_all, chunk_y_all)[0]
+    r_text = 'n={}\nr={:.2f}'.format(chunk_x_all.size, corr_this)
+    ax.text(0, 0.7, r_text, fontsize='medium', horizontalalignment='left')
